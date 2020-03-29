23ba540501d0d31030666d2b9d8b22abd5c3fdfb
Juan Pinto <jpinto@grupov.com.ve>
2020-03-29 01:41:12 -0400

    Adding Promises

--- index.js
+++ index.js
@@ -5,33 +5,32 @@ const dbOperation = require('./operations')
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, { "useUnifiedTopology": "true" }, (error, client) => {
-
-    asssert.equal(error, null);
-
+MongoClient.connect(url, { "useUnifiedTopology": "true" }).then((client) => {
+    
     console.log('Connected to the database');
-
     const database = client.db(dbname);
-    const collection = database.collection('dishes');
 
-    dbOperation.insertDocument(database, { name: "Vadonut", description: "testing" }, 'dishes', (result) => {
-        console.log('Insert Document:\n', result.ops);
-
-        dbOperation.findDocuments(database, 'dishes', (result) => {
+    dbOperation.insertDocument(database, { name: "Vadonut", description: "testing" }, 'dishes')
+        .then((result) => {
+            console.log('Insert Document:\n', result.ops);
+            return dbOperation.findDocuments(database, 'dishes');
+        })
+        .then((documents) => {
+            console.log('Found Documents: \n', documents);
+            return dbOperation.updateDocument(database, { name: "Vadonut" }, { description: "What a vadonut" }, 'dishes');
+        })
+        .then((result) => {
+            console.log('Document Updated:\n', result.result);
+            return dbOperation.findDocuments(database, 'dishes');
+        })
+        .then((result) => {
             console.log('Found Documents: \n', result);
+            return database.dropCollection('dishes');
+        })
+        .then((result) => {
+            console.log('Collection dropped: \n', result);
+            client.close();
+        }).catch((error) => { console.log('Error: ', error) });
 
-            dbOperation.updateDocument(database, { name: "Vadonut" }, { description: "What a vadonut" }, 'dishes', (result) => {
-                console.log('Document Updated:\n', result.result);
-
-                dbOperation.findDocuments(database, 'dishes', (result) => {
-                    console.log('Found Documents: \n', result);
-
-                    database.dropCollection('dishes', (result) => {
-                        console.log('Collection dropped: \n', result);
-                        client.close();
-                    })
-                });
-            });
-        });
-    });
 })
+    .catch((error) => { console.log('Error conecting to Mongo') })
--- operations.js
+++ operations.js
@@ -2,37 +2,42 @@ const assert = require("assert");
 
 exports.insertDocument = (database, document, collectionName, callback) => {
     const dbCollection = database.collection(collectionName);
-    dbCollection.insert(document, (error, result) => {
+    return dbCollection.insert(document)
+    /*, (error, result) => {
         assert.equal(error, null);
         console.log(`Inserted ${result.result.n} documents into the collection`);
         callback(result);
-    })
+    }*/
 };
 
 exports.findDocuments = (database, collectionName, callback) => {
     const dbCollection = database.collection(collectionName);
-    dbCollection.find({}).toArray((error, result) => {
+    return dbCollection.find({}).toArray()
+
+    /*(error, result) => {
         assert.equal(error, null);
         callback(result);
-    })
+    } */
 
 };
 
 exports.removeDocument = (database, document, collectionName, callback) => {
     const dbCollection = database.collection(collectionName);
-    dbCollection.deleteOne(document, (error, result) => {
+    return dbCollection.deleteOne(document)
+    /**, (error, result) => {
         assert.equal(error, null);
         console.log("Document deleted", document);
         callback(result);
-    })
+    } */
 };
 
 
 exports.updateDocument = (database, document, update, collectionName, callback) => {
     const dbCollection = database.collection(collectionName);
-    dbCollection.updateOne(document, { $set: update}, null, (error, result) => {
+    return dbCollection.updateOne(document, { $set: update})
+    /**, (error, result) => {
         assert.equal(error, null);
         console.log("Document updated", document);
         callback(result);
-    })
+    } */
 };
